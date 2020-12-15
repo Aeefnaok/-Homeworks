2a48497d2ddd504b550c3d5787e2cc7b1894a823
Ivan Chupriyanov <Aerocoolbye@yandex.ru>
2020-12-15 23:32:18 +0700

    1 task

--- task_1.py
+++ task_1.py
@@ -1,1 +1,5 @@
-1 print("Hello")
+number = int(input('enter something number '))
+name = input('enter your name ')
+age = int(input('enter your age '))
+
+print(f'Вас зовут {name}, вам {age} лет, вы ввели число {number}')
