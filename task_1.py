5780cbf4d3b7aac894a1b4ee57eaca042023049e
Aeefnaok <72884207+Aeefnaok@users.noreply.github.com>
2021-01-05 22:00:54 +0700

    Delete task_1.py

--- task_1.py
+++
@@ -1,5 +0,0 @@
-number = int(input('enter something number '))
-name = input('enter your name ')
-age = int(input('enter your age '))
-
-print(f'Вас зовут {name}, вам {age} лет, вы ввели число {number}')
